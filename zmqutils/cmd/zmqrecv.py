2e6d9991e4bc4dc2cb89a8e9ae036d15254c5e7e
Lars Kellogg-Stedman <lars@oddbit.com>
2018-03-26 09:22:21 -0400

    added some logging

--- zmqutils/cmd/zmqrecv.py
+++ zmqutils/cmd/zmqrecv.py
@@ -1,27 +1,42 @@
 import click
+import logging
 import sys
 import zmq
 
+LOG = logging.getLogger(__name__)
+
 
 @click.command()
-@click.option('-o', '--output', type=click.File('wb'), default=sys.stdout.buffer)
+@click.option('-o', '--output', type=click.File('wb'),
+              default=sys.stdout.buffer)
 @click.option('-l', '--listen', is_flag=True)
 @click.option('-s', '--subscribe', multiple=True)
+@click.option('-v', '--verbose', 'loglevel', flag_value='INFO', default=True)
+@click.option('-q', '--quiet', 'loglevel', flag_value='WARNING')
 @click.argument('target')
-def cli(output, listen, subscribe, target):
+def cli(output, listen, subscribe, loglevel, target):
+    logging.basicConfig(level=loglevel)
+
     ctx = zmq.Context()
     sock = ctx.socket(zmq.SUB)
 
     if subscribe:
         for s in subscribe:
+            LOG.info('subscribing to %s', s)
             sock.subscribe(s)
     else:
+        LOG.info('subscribing to all messages')
         sock.subscribe('')
 
     if listen:
+        LOG.info('listening on %s', target)
         sock.bind(target)
     else:
+        LOG.info('connecting to %s', target)
         sock.connect(target)
 
+    LOG.info('start receiving')
     while True:
-        output.write(sock.recv())
+        data = sock.recv()
+        output.write(data)
+    LOG.info('finished receiving')
--- zmqutils/cmd/zmqsend.py
+++ zmqutils/cmd/zmqsend.py
@@ -1,21 +1,36 @@
 import click
+import logging
 import sys
 import zmq
 
+LOG = logging.getLogger(__name__)
+
 
 @click.command()
-@click.option('-i', '--input', type=click.File('rb'), default=sys.stdin.buffer)
+@click.option('-i', '--input', type=click.File('rb'),
+              default=sys.stdin.buffer)
 @click.option('-b', '--bufsize', type=int, default=1024)
 @click.option('-l', '--listen', is_flag=True)
+@click.option('-v', '--verbose', 'loglevel', flag_value='INFO', default=True)
+@click.option('-q', '--quiet', 'loglevel', flag_value='WARNING')
 @click.argument('target')
-def cli(input, bufsize, listen, target):
+def cli(input, bufsize, listen, loglevel, target):
+    logging.basicConfig(level=loglevel)
+
     ctx = zmq.Context()
     sock = ctx.socket(zmq.PUB)
 
     if listen:
+        LOG.info('listening on %s', target)
         sock.bind(target)
     else:
+        LOG.info('connecting to %s', target)
         sock.connect(target)
 
+    LOG.info('sending data')
     while True:
-        sock.send(input.read(bufsize))
+        data = input.read(bufsize)
+        if not data:
+            break
+        sock.send(data)
+    LOG.info('finished sending')
